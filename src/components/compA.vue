7d11d8aa2163510ba1987e004134b7d0c26beb37
zoumeihui001 <zoumeihui001@ke.com>
2021-08-16 17:32:49 +0800

    feat: 初始化

---
+++ src/components/compA.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span>{{ msgg }}</span>
+    <button @click="change">点击换字</button>
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comA',
+  props: ['msg'],
+  data() {
+    return {
+      msgg: this.msg,
+      flag: false,
+    }
+  },
+  methods: {
+    change() {
+      this.flag = !this.flag
+      this.msgg = this.flag ? 'this is component A' : '这是组件A'
+    },
+  },
+}
+</script>
---
+++ src/components/compB.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>这是组件B</p>
+</template>
+
+<script>
+export default {
+  name: 'comB',
+  data() {
+    return {}
+  },
+}
+</script>
+<style scoped>
+p{
+  color: chartreuse;
+}
+</style>
